e3713d36eb9cefe2ab16b2ece95e81cfce11b785
Zachary <zachary.kramer@stonybrook.edu>
2024-04-04 17:22:53 -0400

    My first submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,37 @@
 
+#include <stdio.h>
+
+int my_isalpha(char c){
+    return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
+int my_isdigit(char c){
+    return (c >= '0' && c <= '9');
+}
+
+int my_isupper(char c){
+    return (c >= 'A' && c <= 'Z');
+}
+
+char my_toupper(char c){
+    if (c >= 'a' && c <= 'z'){
+        return  c - 'a' + 'A';
+    }
+}
+
+char my_tolower(char c){
+    if (c >= 'A' && c <= 'Z'){
+        return  c - 'A' + 'a';
+    }
+}
+
+int main(){
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c); getchar();
+
+    printf("%d\n", my_isalpha(c));
+    printf("%d\n", my_isdigit(c));
+    printf("%d\n", my_isupper(c));
+    printf("%c\n", my_toupper(c));
+    printf("%c\n", my_tolower(c));
+}
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,44 @@
 
+#include <stdio.h>
+
+
+
+char *MyStrCmp (char str1[], char str2[]){
+    int i; 
+    for (i = 0; str1[i] != '\0' && str2[i] != '\0'; i++){
+        if (str1[i] == str2[i]){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+}
+char *MyStrCmp (char str1[], char str2[]){
+    int i, n; 
+    printf("Enter the number of characters: ");
+    scanf("%c", &n);
+
+    while (i < n){
+        for (i=0; str1[i] != '\0' && str2[i] != '\0';i++){
+            if (str1[i] == str2[i]){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+    }
+
+}
+
+char *MyStrCpy (char str1[], char str2[]){
+    char temp[] = str1[];
+    str1 = str2;
+    printf("%s", temp[]);
+    return temp[];
+}
+
+char *MyStrCat(char str1[], char str2[]){
+    
+}
